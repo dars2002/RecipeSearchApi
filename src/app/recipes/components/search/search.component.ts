392daa80a579ed8bcb2df9c18932852943beee77
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2024-06-13 14:40:13 -0500

    feat: search component

---
+++ src/app/recipes/components/search/search.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter , Output } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AfterViewInit, OnInit } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+import { fromEvent,Subscription } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged} from 'rxjs/operators';
+import { RecipesearchApiService } from '../../services/recipesearch-api.service';
+import {MatIconModule} from '@angular/material/icon';
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule,MatIconModule ],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent implements OnInit,AfterViewInit {
+  @ViewChild('searchInput')
+  inputSearch?:ElementRef
+  @Output() searchQuery = new EventEmitter<string>();
+  constructor(){
+  }
+
+  ngAfterViewInit() {
+      fromEvent<any>(this.inputSearch?.nativeElement,'keyup')
+      .pipe(
+        map(event => event.target.value),
+        debounceTime(500),
+        distinctUntilChanged()
+      ).subscribe(query => this.searchQuery.emit(query));
+  }
+
+  ngOnInit() {
+  }
+
+}
